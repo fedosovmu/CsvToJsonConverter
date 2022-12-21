495fe833c00748f8ca7325207289f90a924486a4
fedosovmu <fedosovmu@gmail.com>
2022-12-21 20:56:57 +0300

    Rename data to translations

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from csv_to_json_converter import CsvToJsonConverter
 
 if __name__ == '__main__':
     converter = CsvToJsonConverter()
-    converter.clear_csv_from_empty_strings('data/translations.csv')
-    converter.convert('data/translations.csv', 'data/ru.json', 'data/en.json')
+    converter.clear_csv_from_empty_strings('translations/translations.csv')
+    converter.convert('translations/translations.csv', 'translations/ru.json', 'translations/en.json')
