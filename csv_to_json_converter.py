ac525e1302a3571a1d16fa6bdf9415e3fe8dfd30
fedosovmu <fedosovmu@gmail.com>
2022-12-21 20:06:24 +0300

    Add encoding='utf-8'

--- csv_to_json_converter.py
+++ csv_to_json_converter.py
@@ -7,13 +7,13 @@ class CsvToJsonConverter:
 
     def clear_csv_from_empty_strings(self, path_to_csv):
         new_file_data=""
-        with open(path_to_csv) as csv_file:
+        with open(path_to_csv, encoding='utf-8') as csv_file:
             for row in csv_file:
                 is_line_empty = row == ',,\n'
                 if not  is_line_empty:
                     new_file_data += row
 
-        with open(path_to_csv, 'w') as csv_file:
+        with open(path_to_csv, 'w', encoding='utf-8') as csv_file:
             csv_file.write(new_file_data)
 
     def convert(self, path_to_csv, path_to_ru_json, path_to_en_json):
@@ -25,7 +25,7 @@ class CsvToJsonConverter:
         print('SUCCESS')
 
     def _load_csv(self, path_to_csv):
-        with open(path_to_csv) as csv_file:
+        with open(path_to_csv, encoding='utf-8') as csv_file:
             csv_reader = csv.DictReader(csv_file)
             return list(csv_reader)
 
@@ -39,6 +39,6 @@ class CsvToJsonConverter:
         return new_dict
 
     def _save_json(self, json_dict, path_to_json):
-        with open(path_to_json, 'w') as json_file:
+        with open(path_to_json, 'w', encoding='utf-8') as json_file:
             json_dump = json.dumps(json_dict, indent=2, ensure_ascii=False)
             json_file.write(str(json_dump).replace('\\\\', '\\'))
